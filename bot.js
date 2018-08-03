aabcdb73c2a14d9ab00056744c26597805bc4fb7
indra424 <41354551+indra424@users.noreply.github.com>
2018-08-03 16:48:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var bot = require("discord-music-bot");
 var serverName = "PRIVATE ROOM";
 var textChannelName = "jennie-room";
 var voiceChannelName = "KPOP AREA";
-var aliasesFile = "aliases.json";
+var aliasesFile = "aliases";
 var botToken = "NDcxOTYyNjkwNDUwMDk2MTI4.DkWmDQ.vYiYZePycJJlfbVesNolQZisFYQ";
 
 bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
